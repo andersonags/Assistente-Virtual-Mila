b3c2fea0eb0c40327910a7c87e981f298f4fbd78
andersonags <djandersongomesdnb@gmail.com>
2022-01-07 20:37:11 -0300

    Corrigindo erro 1

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ r = sr.recognizer()
 
 # Abrir o Microfone para captura
 with sr.Micriphone() as source:
-    audio = r.listem(source) # Define Microfone como fonte de áudio
+    audio = r.listen(source) # Define Microfone como fonte de áudio
     print(r.recognizer_google(audio))
